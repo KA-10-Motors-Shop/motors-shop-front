ce9122390525b5a6526e9fed6b4055a038c6d231
Ana Valeska Santos de Souza <avaleska2908@hotmail.com>
2022-08-04 10:21:12 -0300

    feat: semi-ready home page, lacking the proper structure of the carousel of the listing of ads for cars and motorcycles.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,60 +6,12 @@ import { ProductCard } from "../components/ProductCard";
 import { databaseCar, databaseMoto } from "../database";
 export const Home = () => {   
      
-    return (
-        //<div style={{boxSizing: "border-box"}}>
-        //    <div style={{ height: "5rem", width: "23.4rem", backgroundColor: "#000", color: "#fff" }}>Header (desktop: height: "5rem", width: "100rem")
-        //    </div>
-            
-        //    <div style={{ height: "38.94rem", width: "23.4rem", backgroundColor: "#fff", color: "#000" }}>
-        //        Nav-Menu (desktop: height: "31.43rem", width: "100rem")
-        //    </div>
-            
-        //    <div style={{ height: "33.75rem",  width: "23.4rem", backgroundColor: "#000", color: "#fff"}}>
-        //        <p style={{ height: "1.875rem", width: "23.4rem",  backgroundColor: "#260b92", color: "#fff"}}>Leilão</p>
-                
-        //        <div style={{ display: "flex" }}>
-                    
-        //            <div style={{ display: "flex", flexDirection: "column" }}>
-                        
-        //                <div style={{ height: "27.18rem", width: "20.5rem", backgroundColor: "#a50e0e", color: "#fff", marginRight: "0.5rem", marginLeft: "0.75rem" }}>carro1</div>
-        //                {/*<BrandButton size="lg">bora ver</BrandButton>*/}
-        //                <div style={{ height: "3.8rem", width: "20.5rem", backgroundColor: "#e61abd", color: "#fff", marginRight: "0.5rem", marginLeft: "0.75rem" }}>button1</div>
-                        
-        //            </div>
-                    
-        //            <div style={{ display: "flex", flexDirection: "column" }}>
-                        
-        //                <div style={{ height: "27.18rem", width: "1.68rem", backgroundColor: "#049d41", color: "#000" }}>
-        //                    carro2 (IGNORAR)
-        //                </div>
-        //                <div style={{ height: "3.8rem", width: "1.68rem", backgroundColor: "#e61abd", color: "#fff"}}>button2</div>
-                        
-        //            </div>                    
-                        
-        //        </div>
-                
-        //    </div>
-            
-        //    <div>
-        //            <p>carros</p>
-        //            <div>
-                        
-        //            </div>
-        //        </div>
-        //        <div>
-        //            <p>motos</p>
-        //            <div></div>
-        //        </div>
-        //</div>
-        
+    return (        
         <>
             <Navbar username="Ana" />
             <Flex
                 h={{ base: "623px", lg: "503px" }}
-                //w={"100%"}
                 flexDir={"column"}
-                //mt={{base: "45px", lg: "73px"}}
                 bg="brand.brand2"
                 justifyContent="center"
                 alignItems="center"                
@@ -67,20 +19,15 @@ export const Home = () => {
                 <Heading
                     as="h1"
                     fontSize="lg"
-                    //alignSelf="center"
-                    //alignContent={"center"}
                     mt={{ base: "53px", lg: "80px" }}
                     mb={{ base: "52px", lg: "20px" }}
-                    //ml={{ base: "1.5px", lg: "425px" }}
                     color="greyScale.grey10"
                 >
                     Velocidade e experiência em um lugar feito para você
                 </Heading>     
                 <Text
                     fontSize="xs"
-                    //mt={{ base: "53px", lg: "80px" }}
                     mb={{ base: "52px", lg: "20px" }}
-                    //mx={{ base: "1.5px", lg: "425px" }}
                     color="greyScale.grey10"
                     
                 >
@@ -121,11 +68,8 @@ export const Home = () => {
                 <Heading
                     as="h6"
                     fontSize="lg"
-                    //alignSelf="center"
-                    //alignContent={"center"}
                     mt={{ base: "67px", lg: "200px" }}
                     mb="62px"
-                    //ml={{ base: "1.5px", lg: "425px" }}
                     color="greyScale.grey0"
                 >
                    Carros
@@ -138,11 +82,8 @@ export const Home = () => {
                 <Heading
                         as="h6"
                         fontSize="lg"
-                        //alignSelf="center"
-                        //alignContent={"center"}
                         mt={{ base: "67px", lg: "200px" }}
                         mb="62px"
-                        //ml={{ base: "1.5px", lg: "425px" }}
                         color="greyScale.grey0"
                 >
                     Motos
