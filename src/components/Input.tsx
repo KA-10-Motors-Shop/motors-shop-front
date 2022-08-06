57e64af39b1d451282a25f92a63525cc1e40ad46
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-08-06 13:52:54 -0300

    fix: split Input component into input and textarea for better prop handling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,46 +1,52 @@
 import {
-  Input as ChakraInput,
   FormLabel,
   FormControl,
+  Input as ChakraInput,
   Textarea as ChakraTextarea,
+  InputProps as ChakraInputProps,
+  TextareaProps as ChakraTextareaProps,
 } from "@chakra-ui/react";
 
-interface InputProps {
-  type: "input" | "textarea";
-  label: string;
-  placeholder: string;
+interface InputProps extends ChakraInputProps {
+  label?: string;
 }
 
-export const Input = ({ label, type, placeholder, ...rest }: InputProps) => {
-  const props = {
-    fontSize: "sm",
-    placeholder: placeholder,
-    color: "greyScale.grey3",
-    border: "2px solid",
-    borderColor: "greyScale.grey8",
-    borderRadius: "4px",
-    px: "16px",
-    _hover: {
-      bg: "greyScale.grey8",
-    },
-    _focus: {
-      bg: "greyScale.grey9",
-      borderColor: "#5126EA",
-      color: "greyScale.grey1",
-      boxShadow: "none",
-    },
-  };
+interface TextareaProps extends ChakraTextareaProps {
+  label?: string;
+}
 
-  return (
-    <FormControl px="20px">
-      <FormLabel fontSize="xs" fontWeight="500">
-        {label}
-      </FormLabel>
-      {type === "input" ? (
-        <ChakraInput {...props} {...rest} />
-      ) : (
-        <ChakraTextarea {...props} {...rest} />
-      )}
-    </FormControl>
-  );
+const props = {
+  fontSize: "sm",
+  color: "greyScale.grey3",
+  border: "2px solid",
+  borderColor: "greyScale.grey8",
+  borderRadius: "4px",
+  px: "16px",
+  _hover: {
+    bg: "greyScale.grey8",
+  },
+  _focus: {
+    bg: "greyScale.grey9",
+    borderColor: "#5126EA",
+    color: "greyScale.grey1",
+    boxShadow: "none",
+  },
 };
+
+export const Input = ({ label, type, ...rest }: InputProps) => (
+  <FormControl px="20px">
+    <FormLabel fontSize="xs" fontWeight="500">
+      {label}
+    </FormLabel>
+    <ChakraInput {...props} {...rest} />
+  </FormControl>
+);
+
+export const Textarea = ({ label, ...rest }: TextareaProps) => (
+  <FormControl>
+    <FormLabel fontSize="xs" fontWeight="500">
+      {label}
+    </FormLabel>
+    <ChakraTextarea {...props} {...rest} />
+  </FormControl>
+);
