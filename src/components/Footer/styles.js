3d87b2f4f013f3c17df0b6350716a5b731fa90d4
Nicollas Anderson <nicollas_sdias@hotmail.com>
2022-09-06 11:14:09 -0300

    fix: fixed some bugs in home page and fixed header for desktop

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,48 +1,48 @@
 import styled from "styled-components";
 
 export const FooterStyled = styled.footer`
-	width: 100vw;
-	height: 19.4rem;
-	background-color: var(--grey0);
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+  width: 100%;
+  height: 19.4rem;
+  background-color: var(--grey0);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-	img {
-		width: 9.56em;
-		height: 1.65em;
-	}
+  img {
+    width: 9.56em;
+    height: 1.65em;
+  }
 
-	p {
-		margin-top: 3.75em;
-		margin-bottom: 3.75em;
-		font-size: 0.875em;
-		line-height: 1.5em;
-		color: var(--whiteFixed);
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+  p {
+    margin-top: 3.75em;
+    margin-bottom: 3.75em;
+    font-size: 0.875em;
+    line-height: 1.5em;
+    color: var(--whiteFixed);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-	button {
-		width: 3.3125em;
-		height: 3.125em;
-		border-radius: 4px;
-		background-color: var(--grey1);
-		border: 0;
+  button {
+    width: 3.3125em;
+    height: 3.125em;
+    border-radius: 4px;
+    background-color: var(--grey1);
+    border: 0;
 
-		svg {
-			color: var(--whiteFixed);
-			size: 1rem;
-		}
-	}
+    svg {
+      color: var(--whiteFixed);
+      size: 1rem;
+    }
+  }
 
-	@media (min-width: 750px) {
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-		padding-left: 3.68rem;
-		padding-right: 3.68rem;
-	}
+  @media (min-width: 750px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 3.68rem;
+    padding-right: 3.68rem;
+  }
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,70 +1,86 @@
 import { useEffect, useRef, useState } from "react";
 import Logo from "../../assets/Logo.svg";
-import { Outline2Button } from "../Button/Button";
-import { HeaderStyled } from "./styles";
+import {
+  LightButton,
+  Outline2Button,
+  OutlineLightButton,
+} from "../Button/Button";
+import { NavLinks, HeaderStyled, DivLoginButton } from "./styles";
 import { HiOutlineMenu } from "react-icons/hi";
 import { IoMdClose } from "react-icons/io";
 
 export const Header = () => {
-	const [isLogged, setIsLogged] = useState(false);
-	const [menuIsVisible, setMenuIsVisible] = useState(false);
-	const handleLogin = () => setIsLogged(!isLogged);
+  const [isLogged, setIsLogged] = useState(false);
+  const [menuIsVisible, setMenuIsVisible] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const handleLogin = () => setIsLogged(!isLogged);
 
-	const popUpMenu = [
-		"Editar Perfil",
-		"Editar Endereço",
-		"Minhas compras",
-		"Sair",
-	];
+  const popUpMenu = [
+    "Editar Perfil",
+    "Editar Endereço",
+    "Minhas compras",
+    "Sair",
+  ];
 
-	return (
-		<HeaderStyled>
-			<nav className="header-menu">
-				<section className="header-toggle">
-					<img src={Logo} alt="Logo da Motors Shop" />
-					{menuIsVisible ? (
-						<div
-							className="header-menu__toggle header-menu--close"
-							onClick={() => setMenuIsVisible(false)}
-						>
-							<IoMdClose size={24} />
-						</div>
-					) : (
-						<div className="header-menu__toggle header-menu--open">
-							<HiOutlineMenu
-								size={24}
-								onClick={() => setMenuIsVisible(true)}
-							/>
-						</div>
-					)}
-				</section>
-				{menuIsVisible && (
-					<section className="header-mobile">
-						<ul className="header-mobile__list header-mobile__list--breadcrumbs">
-							<li>
-								<a href="#cars">Carros</a>
-							</li>
-							<li>
-								<a href="#motorcycle">Motos</a>
-							</li>
-							<li>
-								<a href="#auction">Leilão</a>
-							</li>
-						</ul>
-						<div className="header-div__line"></div>
-						<ul className="header-mobile__list header-mobile__list--sign">
-							<li>
-								<a className="header-bottom__login">
-									Fazer Login
-								</a>
-							</li>
-							<li>
-								<Outline2Button>Cadastrar</Outline2Button>
-							</li>
-						</ul>
-					</section>
-				)}
-			</nav>
-		</HeaderStyled>
-	);
+  window.onresize = window.onload = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  return (
+    <HeaderStyled>
+      <nav className="header-menu">
+        <section className="header-toggle">
+          <img src={Logo} alt="Logo da Motors Shop" />
+          {screenWidth > 900 ? (
+            <NavLinks>
+              <nav>
+                <a href="#cars">Carros</a>
+                <a href="#cars">Motos</a>
+                <a href="#cars">Leilão</a>
+              </nav>
+              <DivLoginButton>
+                <LightButton>Login</LightButton>
+                <OutlineLightButton>Cadastrar</OutlineLightButton>
+              </DivLoginButton>
+            </NavLinks>
+          ) : menuIsVisible ? (
+            <div
+              className="header-menu__toggle header-menu--close"
+              onClick={() => setMenuIsVisible(false)}
+            >
+              <IoMdClose size={24} />
+            </div>
+          ) : (
+            <div className="header-menu__toggle header-menu--open">
+              <HiOutlineMenu size={24} onClick={() => setMenuIsVisible(true)} />
+            </div>
+          )}
+        </section>
+        {menuIsVisible && (
+          <section className="header-mobile">
+            <ul className="header-mobile__list header-mobile__list--breadcrumbs">
+              <li>
+                <a href="#cars">Carros</a>
+              </li>
+              <li>
+                <a href="#motorcycle">Motos</a>
+              </li>
+              <li>
+                <a href="#auction">Leilão</a>
+              </li>
+            </ul>
+            <div className="header-div__line"></div>
+            <ul className="header-mobile__list header-mobile__list--sign">
+              <li>
+                <button className="header-bottom__login">Fazer Login</button>
+              </li>
+              <li>
+                <Outline2Button>Cadastrar</Outline2Button>
+              </li>
+            </ul>
+          </section>
+        )}
+      </nav>
+    </HeaderStyled>
+  );
 };
