cfa1e05b8ca87cc0108e17182276af1de73fdf7a
Ana Valeska Santos de Souza <avaleska2908@hotmail.com>
2022-09-09 15:52:07 -0300

    refactor/fix: adjusting stylizations of the login buttons and anchors.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import Logo from "../../assets/Logo.svg";
 import {
-  LightButton,
-  Outline2Button,
-  OutlineLightButton,
+	LightButton,
+	Outline2Button,
+	OutlineLightButton,
 } from "../Button/Button";
 import { NavLinks, HeaderStyled, DivLoginButton } from "./styles";
 import { HiOutlineMenu } from "react-icons/hi";
@@ -14,95 +14,110 @@ import { useHistory } from "react-router";
 import CreateAdModal from "../CreateAdModal";
 
 export const Header = () => {
-  const [isLogged, setIsLogged] = useState(false);
-  const [menuIsVisible, setMenuIsVisible] = useState(false);
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+	const [isLogged, setIsLogged] = useState(false);
+	const [menuIsVisible, setMenuIsVisible] = useState(false);
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-  const handleLogin = () => setIsLogged(!isLogged);
+	const handleLogin = () => setIsLogged(!isLogged);
 
-  const { token }: any = UseTokenProvider();
+	const { token }: any = UseTokenProvider();
 
-  const popUpMenu = [
-    "Editar Perfil",
-    "Editar Endereço",
-    "Minhas compras",
-    "Sair",
-  ];
+	const popUpMenu = [
+		"Editar Perfil",
+		"Editar Endereço",
+		"Minhas compras",
+		"Sair",
+	];
 
-  window.onresize = window.onload = () => {
-    setScreenWidth(window.innerWidth);
-  };
+	window.onresize = window.onload = () => {
+		setScreenWidth(window.innerWidth);
+	};
 
-  const history = useHistory();
+	const history = useHistory();
 
-  return (
-    <HeaderStyled>
-      <nav className="header-menu">
-        <section className="header-toggle">
-          <img
-            src={Logo}
-            alt="Logo da Motors Shop"
-            onClick={() => history.push("/")}
-          />
-          {screenWidth > 900 ? (
-            <NavLinks token={token}>
-              <nav>
-                <a href="#cars">Carros</a>
-                <a href="#cars">Motos</a>
-                <a href="#cars">Leilão</a>
-              </nav>
+	return (
+		<HeaderStyled>
+			<nav className="header-menu">
+				<section className="header-toggle">
+					<img
+						src={Logo}
+						alt="Logo da Motors Shop"
+						onClick={() => history.push("/")}
+					/>
+					{screenWidth > 900 ? (
+						<NavLinks token={token}>
+							<nav>
+								<a href="#cars">Carros</a>
+								<a href="#cars">Motos</a>
+								<a href="#cars">Leilão</a>
+							</nav>
 
-              <DivLoginButton>
-                {token ? (
-                  <Avatar token={token} size="32px" bigAvatar={false}></Avatar>
-                ) : (
-                  <>
-                    <LightButton onClick={() => history.push("/login")}>
-                      Login
-                    </LightButton>
-                    <OutlineLightButton>Cadastrar</OutlineLightButton>
-                  </>
-                )}
-              </DivLoginButton>
-            </NavLinks>
-          ) : menuIsVisible ? (
-            <div
-              className="header-menu__toggle header-menu--close"
-              onClick={() => setMenuIsVisible(false)}
-            >
-              <IoMdClose size={24} />
-            </div>
-          ) : (
-            <div className="header-menu__toggle header-menu--open">
-              <HiOutlineMenu size={24} onClick={() => setMenuIsVisible(true)} />
-            </div>
-          )}
-        </section>
-        {menuIsVisible && (
-          <section className="header-mobile">
-            <ul className="header-mobile__list header-mobile__list--breadcrumbs">
-              <li>
-                <a href="#cars">Carros</a>
-              </li>
-              <li>
-                <a href="#motorcycle">Motos</a>
-              </li>
-              <li>
-                <a href="#auction">Leilão</a>
-              </li>
-            </ul>
-            <div className="header-div__line"></div>
-            <ul className="header-mobile__list header-mobile__list--sign">
-              <li>
-                <button className="header-bottom__login">Fazer Login</button>
-              </li>
-              <li>
-                <Outline2Button>Cadastrar</Outline2Button>
-              </li>
-            </ul>
-          </section>
-        )}
-      </nav>
-    </HeaderStyled>
-  );
+							<DivLoginButton>
+								{token ? (
+									<Avatar
+										token={token}
+										size="32px"
+										bigAvatar={false}
+									></Avatar>
+								) : (
+									<>
+										<LightButton
+											onClick={() =>
+												history.push("/login")
+											}
+										>
+											Login
+										</LightButton>
+										<OutlineLightButton>
+											Cadastrar
+										</OutlineLightButton>
+									</>
+								)}
+							</DivLoginButton>
+						</NavLinks>
+					) : menuIsVisible ? (
+						<div
+							className="header-menu__toggle header-menu--close"
+							onClick={() => setMenuIsVisible(false)}
+						>
+							<IoMdClose size={24} />
+						</div>
+					) : (
+						<div className="header-menu__toggle header-menu--open">
+							<HiOutlineMenu
+								size={24}
+								onClick={() => setMenuIsVisible(true)}
+							/>
+						</div>
+					)}
+				</section>
+				{menuIsVisible && (
+					<section className="header-mobile">
+						<ul className="header-mobile__list header-mobile__list--breadcrumbs">
+							<li>
+								<a href="#cars">Carros</a>
+							</li>
+							<li>
+								<a href="#motorcycle">Motos</a>
+							</li>
+							<li>
+								<a href="#auction">Leilão</a>
+							</li>
+						</ul>
+						<div className="header-div__line"></div>
+						<ul className="header-mobile__list header-mobile__list--sign">
+							<li>
+								<button className="header-bottom__login">
+									Fazer Login
+								</button>
+							</li>
+							<li>
+								<Outline2Button>Cadastrar</Outline2Button>
+							</li>
+						</ul>
+					</section>
+				)}
+			</nav>
+		</HeaderStyled>
+	);
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,108 +1,108 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  section.main-coverPage {
-    display: flex;
-    flex-direction: column;
-    padding-top: 8.3125rem;
-    background-color: var(--brand2);
-    align-items: center;
-    width: 100%;
-    padding-bottom: 5.3125rem;
+	section.main-coverPage {
+		display: flex;
+		flex-direction: column;
+		padding-top: 8.3125rem;
+		background-color: var(--brand2);
+		align-items: center;
+		width: 100%;
+		padding-bottom: 5.3125rem;
 
-    h3 {
-      font-size: 2rem;
-      line-height: 2.5rem;
-      text-align: center;
-      margin-bottom: 3.25rem;
-      color: var(--grey10);
-      width: 23.5rem;
-      height: 7.5rem;
+		h3 {
+			font-size: 2rem;
+			line-height: 2.5rem;
+			text-align: center;
+			margin-bottom: 3.25rem;
+			color: var(--grey10);
+			width: 23.5rem;
+			height: 7.5rem;
 
-      @media (min-width: 750px) {
-        width: 35rem;
-        margin-bottom: 0.5rem;
-      }
-    }
+			@media (min-width: 750px) {
+				width: 35rem;
+				margin-bottom: 0.5rem;
+			}
+		}
 
-    p {
-      text-align: center;
-      color: var(--grey9);
-      margin-bottom: 3.25rem;
-      line-height: 1.75rem;
-    }
+		p {
+			text-align: center;
+			color: var(--grey9);
+			margin-bottom: 3.25rem;
+			line-height: 1.75rem;
+		}
 
-    div {
-      display: flex;
-      flex-direction: column;
+		div {
+			display: flex;
+			flex-direction: column;
 
-      button {
-        width: 21.93rem;
-        height: 3rem;
-        margin-bottom: 1.25rem;
-        color: var(--grey10);
-        font-size: 1rem;
-        font-weight: 600;
-      }
+			button {
+				width: 21.93rem;
+				height: 3rem;
+				margin-bottom: 1.25rem;
+				color: var(--grey10);
+				font-size: 1rem;
+				font-weight: 600;
+			}
 
-      @media (min-width: 750px) {
-        flex-direction: row;
+			@media (min-width: 750px) {
+				flex-direction: row;
 
-        button {
-          width: 7.34rem;
-          margin-right: 1.3125rem;
-        }
-      }
-    }
-  }
+				button {
+					width: 7.34rem;
+					margin-right: 1.3125rem;
+				}
+			}
+		}
+	}
 `;
 
 export const AuctionBox = styled.section`
-  height: 37.52rem;
-  width: 100%;
+	height: 37.52rem;
+	width: 100%;
 
-  h5 {
-    margin-top: 6rem;
-    margin-bottom: 5.92rem;
-    margin-left: 0.75rem;
-    font-size: 1.5rem;
-  }
+	h5 {
+		margin-top: 6rem;
+		margin-bottom: 5.92rem;
+		margin-left: 0.75rem;
+		font-size: 1.5rem;
+	}
 
-  ul {
-    display: flex;
-    overflow-x: auto;
-    scroll-behavior: smooth;
-    height: 500px;
-    width: 100%;
+	ul {
+		display: flex;
+		overflow-x: auto;
+		scroll-behavior: smooth;
+		height: 500px;
+		width: 100%;
 
-    li:first-child {
-      margin-left: 0.75rem;
-    }
-  }
+		li:first-child {
+			margin-left: 0.75rem;
+		}
+	}
 `;
 
 export const SaleBox = styled.section`
-  height: 32.8125rem;
-  width: 100%;
+	height: 32.8125rem;
+	width: 100%;
 
-  h5 {
-    margin-top: 4.1875rem;
-    margin-bottom: 2.5rem;
-    font-weight: 600;
-    font-size: 1.5rem;
-    line-height: 1.875rem;
-    color: var(--allBlack);
-    margin-left: 1.4375rem;
-  }
+	h5 {
+		margin-top: 4.1875rem;
+		margin-bottom: 2.5rem;
+		font-weight: 600;
+		font-size: 1.5rem;
+		line-height: 1.875rem;
+		color: var(--allBlack);
+		margin-left: 1.4375rem;
+	}
 
-  ul {
-    display: flex;
-    overflow-x: auto;
-    scroll-behavior: smooth;
-    height: 26.5rem;
+	ul {
+		display: flex;
+		overflow-x: auto;
+		scroll-behavior: smooth;
+		height: 26.5rem;
 
-    li:first-child {
-      margin-left: 1.4375rem;
-    }
-  }
+		li:first-child {
+			margin-left: 1.4375rem;
+		}
+	}
 `;
